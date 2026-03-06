68590201cc82095c27036c3420e5b38b39494f10
John Kealy <johnckealy.dev@gmail.com>
2026-03-06 10:02:36 +0000

    remove dependence on UID in payload for delete endpoint

--- app/routes/auth_router.py
+++ app/routes/auth_router.py
@@ -6,6 +6,6 @@ from app.core.auth import delete_auth_user, get_current_user
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
-@router.delete("/users/{user_id}")
-async def delete_user(user_id: str, _: User = Depends(get_current_user)) -> dict:
-    return await delete_auth_user(user_id)
+@router.delete("/users/me")
+async def delete_user(user: User = Depends(get_current_user)) -> dict:
+    return await delete_auth_user(user.id)
